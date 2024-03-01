9cf9f2cba131ef6ae1840f1b6c5a11c89fe3cee5
Ubuntu <ubuntu@ip-172-31-38-237.ap-northeast-2.compute.internal>
2024-03-01 05:22:25 +0000

    FEAT chat models and remove situation

--- app/schema/chat_schema.py
+++ app/schema/chat_schema.py
@@ -6,8 +6,8 @@ class ChatCreateRequest(BaseModel):
                     description="사용자 id")
     character_id: str = Field(..., example="65dac988d53681899908cc23", 
                     description="캐릭터 id")
-    situation_id: str = Field(..., example="65dabc6d6af9f66286b55ddd", 
-                    description="상황 id")
+    # situation_id: str = Field(..., example="65dabc6d6af9f66286b55ddd", 
+    #                 description="상황 id")
     # name: str = Field(..., example="엘사", 
     #                 description="캐릭터 이름")
     # setting: Optional[str] = Field(..., example="디즈니 애니메이션 겨울왕국 주인공/ 얼음 마법사 / 안나의 친언니", 
