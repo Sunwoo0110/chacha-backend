6ad180da61576606301dce9ff5e64d979b161876
sunwookim <sunkim09@g.skku.edu>
2024-03-06 12:42:33 +0900

    FEAT uvicorn bash

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd app
 
-uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2 --timeout-keep-alive 30 --keep-alive 10
+uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2 --timeout-keep-alive 60 --keep-alive 30
